1893bd5849e78fe542d4189f4e0aef0432672228
oncleben31 <oncleben31@users.noreply.github.com>
2020-07-27 08:35:19 +0200

    Add lint check in nox sessions

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,8 @@
 """Nox sessions."""
 import nox
 
+locations = "src", "tests", "noxfile.py"
+
 
 @nox.session(python=["3.8", "3.7", "3.6"])
 def tests(session):
@@ -8,3 +10,11 @@ def tests(session):
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
+
+
+@nox.session(python=["3.8", "3.7", "3.6"])
+def lint(session):
+    """Ling using flake8."""
+    args = session.posargs or locations
+    session.run("poetry", "install", external=True)
+    session.run("flake8", *args)
