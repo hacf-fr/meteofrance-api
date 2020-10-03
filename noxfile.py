d12530e3222eed14f3704972faf1b226c1745d24
oncleben31 <oncleben31@users.noreply.github.com>
2020-10-03 23:32:46 +0200

    Add nox session to generate docs

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,6 @@
 """Nox sessions."""
 import contextlib
+import shutil
 import sys
 import tempfile
 from pathlib import Path
@@ -12,7 +13,7 @@ from nox.sessions import Session
 python_versions = ["3.8", "3.7", "3.6"]
 package = "meteofrance_api"
 nox.options.sessions = "pre-commit", "safety", "mypy", "tests", "typeguard"
-locations = "src", "tests", "noxfile.py"
+locations = "src", "tests", "noxfile.py", "docs/conf.py"
 
 
 class Poetry:
@@ -227,3 +228,31 @@ def coverage(session: Session) -> None:
         session.run("coverage", "combine")
 
     session.run("coverage", *args)
+
+
+@nox.session(name="docs-build", python="3.8")
+def docs_build(session: Session) -> None:
+    """Build the documentation."""
+    args = session.posargs or ["docs", "docs/_build"]
+    session.install(".")
+    session.install("sphinx")
+
+    build_dir = Path("docs", "_build")
+    if build_dir.exists():
+        shutil.rmtree(build_dir)
+
+    session.run("sphinx-build", *args)
+
+
+@nox.session(python="3.8")
+def docs(session: Session) -> None:
+    """Build and serve the documentation with live reloading on file changes."""
+    args = session.posargs or ["--open-browser", "docs", "docs/_build"]
+    session.install(".")
+    session.install("sphinx", "sphinx-autobuild")
+
+    build_dir = Path("docs", "_build")
+    if build_dir.exists():
+        shutil.rmtree(build_dir)
+
+    session.run("sphinx-autobuild", *args)
