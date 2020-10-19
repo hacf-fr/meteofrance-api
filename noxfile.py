90288bcceb3cb1acd2fa5a7fcb0061656779a83e
oncleben31 <oncleben31@users.noreply.github.com>
2020-10-19 10:05:42 +0200

    Fix noxfile.py
    Add requests_mock dependencie and fix package name.

--- noxfile.py
+++ noxfile.py
@@ -9,7 +9,7 @@ import nox_poetry.patch
 from nox.sessions import Session
 
 
-package = "meteofrance_api"
+package = "meteofrance"
 python_versions = ["3.9", "3.8", "3.7", "3.6"]
 nox.options.sessions = (
     "pre-commit",
@@ -118,7 +118,7 @@ def mypy(session: Session) -> None:
 def tests(session: Session) -> None:
     """Run the test suite."""
     session.install(".")
-    session.install("coverage[toml]", "pytest", "pygments")
+    session.install("coverage[toml]", "pytest", "pygments", "requests_mock")
     try:
         session.run("coverage", "run", "--parallel", "-m", "pytest", *session.posargs)
     finally:
@@ -145,7 +145,7 @@ def coverage(session: Session) -> None:
 def typeguard(session: Session) -> None:
     """Runtime type checking using Typeguard."""
     session.install(".")
-    session.install("pytest", "typeguard", "pygments")
+    session.install("pytest", "typeguard", "pygments", "requests_mock")
     session.run("pytest", f"--typeguard-packages={package}", *session.posargs)
 
 
