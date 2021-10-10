aa9ecbcf7c53f5df79e2284a1cb777673063f353
oncleben31 <oncleben31@users.noreply.github.com>
2021-10-10 16:03:12 +0200

    Exception name chage to have Error suffix

--- tests/test_exception.py
+++ tests/test_exception.py
@@ -2,11 +2,11 @@
 """Tests for meteofrance module. Exception classes."""
 import pytest
 
-from meteofrance_api.exceptions import MeteoFranceException
+from meteofrance_api.exceptions import MeteoFranceError
 
 
 def test_meteofrance_exception() -> None:
-    """Tests MeteoFranceException excetion."""
-    with pytest.raises(MeteoFranceException):
+    """Tests MeteoFranceError exception."""
+    with pytest.raises(MeteoFranceError):
         # TODO test for coverage. To be update in the future.
-        raise MeteoFranceException("Test Error")
+        raise MeteoFranceError("Test Error")
