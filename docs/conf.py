f32d2f38466607394dd7bceedf21bc5a4550657d
Quentame <polletquentin74@me.com>
2023-10-23 22:28:26 +0200

    Theme is furo

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ copyright = f"{datetime.now().year}, {author}"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
-    "sphinx_rtd_theme",
+    "furo",
 ]
 autodoc_typehints = "description"
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
