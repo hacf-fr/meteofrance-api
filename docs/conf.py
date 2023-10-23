b51da027b9721ff97b055da56a1cfe76b89edf34
Quentame <polletquentin74@me.com>
2023-10-23 22:36:50 +0200

    furo in not an extension

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,6 @@ copyright = f"{datetime.now().year}, {author}"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
-    "furo",
 ]
 autodoc_typehints = "description"
 html_theme = "furo"
