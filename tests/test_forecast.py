e46665b6c49514b3f7db1cfe49e0b9ef26621d9b
François AIZPURU <francois.aizpuru@gmail.com>
2023-11-29 16:35:28 +0100

    replace type== by isinstance (#681)
    Co-authored-by: François AIZPURU <francois.aizpuru@eukleia.eu>

--- tests/test_forecast.py
+++ tests/test_forecast.py
@@ -15,19 +15,17 @@ def test_forecast_france() -> None:
     weather_forecast = client.get_forecast(latitude=48.8075, longitude=2.24028)
     now_ts = int(time.time())
 
-    assert type(weather_forecast.position) == dict
-    assert type(weather_forecast.updated_on) == int
+    assert isinstance(weather_forecast.position, dict)
+    assert isinstance(weather_forecast.updated_on, int)
     assert "T" in weather_forecast.daily_forecast[0].keys()
     assert "humidity" in weather_forecast.daily_forecast[0].keys()
     assert "rain" in weather_forecast.probability_forecast[0].keys()
     assert "clouds" in weather_forecast.forecast[0].keys()
-    assert (
-        type(
-            weather_forecast.timestamp_to_locale_time(
-                weather_forecast.daily_forecast[0]["dt"]
-            )
-        )
-        == datetime
+    assert isinstance(
+        weather_forecast.timestamp_to_locale_time(
+            weather_forecast.daily_forecast[0]["dt"]
+        ),
+        datetime,
     )
     assert abs(weather_forecast.nearest_forecast["dt"] - now_ts) <= 30 * 60
     assert now_ts - 3600 <= weather_forecast.current_forecast["dt"] <= now_ts
@@ -44,19 +42,17 @@ def test_forecast_world() -> None:
     weather_forecast = client.get_forecast(latitude=45.5016889, longitude=73.567256)
     now_ts = int(time.time())
 
-    assert type(weather_forecast.position) == dict
-    assert type(weather_forecast.updated_on) == int
+    assert isinstance(weather_forecast.position, dict)
+    assert isinstance(weather_forecast.updated_on, int)
     assert "T" in weather_forecast.daily_forecast[0].keys()
     assert "humidity" in weather_forecast.daily_forecast[0].keys()
     assert not weather_forecast.probability_forecast
     assert "clouds" in weather_forecast.forecast[0].keys()
-    assert (
-        type(
-            weather_forecast.timestamp_to_locale_time(
-                weather_forecast.daily_forecast[0]["dt"]
-            )
-        )
-        == datetime
+    assert isinstance(
+        weather_forecast.timestamp_to_locale_time(
+            weather_forecast.daily_forecast[0]["dt"]
+        ),
+        datetime,
     )
     assert abs(weather_forecast.nearest_forecast["dt"] - now_ts) == min(
         abs(x["dt"] - now_ts) for x in weather_forecast.forecast
@@ -77,17 +73,15 @@ def test_forecast_place() -> None:
 
     weather_forecast = client.get_forecast_for_place(place=Place(MOUNTAIN_CITY))
 
-    assert type(weather_forecast.position) == dict
-    assert type(weather_forecast.updated_on) == int
+    assert isinstance(weather_forecast.position, dict)
+    assert isinstance(weather_forecast.updated_on, int)
     assert "T" in weather_forecast.daily_forecast[0].keys()
     assert "humidity" in weather_forecast.daily_forecast[0].keys()
     assert "rain" in weather_forecast.probability_forecast[0].keys()
     assert "clouds" in weather_forecast.forecast[0].keys()
-    assert (
-        type(
-            weather_forecast.timestamp_to_locale_time(
-                weather_forecast.daily_forecast[0]["dt"]
-            )
-        )
-        == datetime
+    assert isinstance(
+        weather_forecast.timestamp_to_locale_time(
+            weather_forecast.daily_forecast[0]["dt"]
+        ),
+        datetime,
     )
